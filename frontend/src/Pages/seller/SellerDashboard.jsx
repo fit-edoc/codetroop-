b8b390753cbbc68b7b84da1838dbaa7569530a76
fitman <fitmanverma@gmail.com>
2025-07-28 16:39:33 +0530

    done

--- frontend/src/Pages/seller/SellerDashboard.jsx
+++ frontend/src/Pages/seller/SellerDashboard.jsx
@@ -1,7 +1,26 @@
 
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 const SellerDashboard = () => {
+
+  const getProfile =async()=>{
+
+    try {
+
+      const res = await axios.get('/api/seller/profile')
+
+      console.log(res.data)
+      
+    } catch (error) {
+      
+    }
+  }
+
+
+  useEffect(()=>{
+    getProfile()
+  },[])
   return (
     <div>
       seller dash
--- frontend/src/Pages/venders/VenderDashboard.jsx
+++ frontend/src/Pages/venders/VenderDashboard.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const VenderDashboard = () => {
+
+
+  const [profile,setProfile] = useState([])
+
+const getProfile = async()=>{
+
+  try {
+    
+   const res =  await axios.get("/api/vendor/profile")
+
+   console.log(res.data)
+  } catch (error) {
+    
+  }
+}
+
+
+useEffect(()=>{
+  getProfile()
+},[])
+
   return (
     <div>
       vendrer
